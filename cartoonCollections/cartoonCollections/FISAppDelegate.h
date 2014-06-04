9269c60efdca5942a2ac071af2c06891f4c517b6
Joe Burgess <joe@flatironschool.com>
2014-06-04 09:20:57 -0400

    Adding git ignore

---
+++ cartoonCollections/cartoonCollections/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  cartoonCollections
+//
+//  Created by Joe Burgess on 6/4/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
